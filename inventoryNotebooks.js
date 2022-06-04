1dac4618b7d5527e8fb03677dbd9b782cc7a6f9e
Terry Babyuk <tbabyuk@gmail.com>
2022-06-04 19:14:19 -0400

    updated item prices, cleaned up js code

--- inventoryNotebooks.js
+++ inventoryNotebooks.js
@@ -8,10 +8,9 @@ class Notebook {
   }
 
 
-
 const notebook_dictation_48 = new Notebook('Music Dictation Book - 48 pgs', 3.30, 0.05, 2);
 
-
+//Array with just one item for now, more to be added in the future
 export const notebooksArray = [
     notebook_dictation_48
 ]
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { booksArray } from './inventoryBooks.js';
 import { notebooksArray } from './inventoryNotebooks.js';
 
-//SELECT DOM ELEMENTS
 
+//SELECT DOM ELEMENTS
 //Input Fields
 const inputSubtotal = document.querySelector('#input-subtotal');
 const selectTax = document.querySelector('#select-tax');
@@ -14,8 +14,7 @@ const showSubtotal = document.querySelector('#show-subtotal');
 const showTax = document.querySelector('#show-tax');
 const showTotal1 = document.querySelector('#show-total-1');
 const showTotal2 = document.querySelector('#show-total-2');
-const showInventory = document.querySelector('#show-remaining-inventory');
-const modal = document.querySelector('#modal');
+const showRemainingStock = document.querySelector('#show-remaining-stock');
 
 //Buttons
 const btnCalcTotal = document.querySelector('#btn-calc-total');
@@ -24,11 +23,10 @@ const btnShowBooks = document.querySelector('#btn-inventory-books');
 const btnShowNotebooks = document.querySelector('#btn-inventory-notebooks');
 const btnShowOther = document.querySelector('#btn-inventory-other');
 const btnSalesLog = document.querySelector('#btn-sales-log');
-const btnResetLog = document.querySelector('#btn-sales-reset-log')
-
-
+const btnResetSalesLog = document.querySelector('#btn-sales-reset-log');
 
 //Other Elements
+const modal = document.querySelector('#modal');
 const modalOverlay = document.querySelector('#modal-overlay');
 
 
@@ -36,32 +34,25 @@ const modalOverlay = document.querySelector('#modal-overlay');
 
 // EVENT LISTENERS
 
-//Buttons
 btnCalcTotal.addEventListener('click', calcTotal);
 btnShowTotal.addEventListener('click', showItemInfo);
-btnShowBooks.addEventListener('click', showModal);
-btnShowNotebooks.addEventListener('click', showModal2);
+btnShowBooks.addEventListener('click', showBookModal);
+btnShowNotebooks.addEventListener('click', showNotebookModal);
+btnSalesLog.addEventListener('click', showSalesModal);
+btnResetSalesLog.addEventListener('click', resetSalesLog);
 btnShowOther.addEventListener('click', nothingToShow);
-btnSalesLog.addEventListener('click', showModal4);
-btnResetLog.addEventListener('click', resetSalesLog);
-
-document.addEventListener('click', (e) => {
-  if(e.target.className === 'btn-subtract-qty') {
-  updateStorage(e)
-  }
-}
-);
-
 inputSubtotal.addEventListener('click', clearSubtotal);
 modalOverlay.addEventListener('click', hideModal);
 selectItem.addEventListener('click', clearItemInfoFields);
+document.addEventListener('click', e => updateStorage(e));
 
 
 //===================================================================================//
 
-//FUNCTIONS
 
-//Show Date (temporarily commented out)
+//CODE BY SECTIONS (TOP, MIDDLE, BOTTOM)
+
+//TOP SECTION CODE
 
 const date = new Date();
 const dateFormatted = date.toLocaleString('en-US', {
@@ -71,25 +62,16 @@ const dateFormatted = date.toLocaleString('en-US', {
   year: 'numeric',
 });
 
-function showDate() {
+(function showDate() {
   currentDate.innerText = dateFormatted;
-}
-
-showDate();
-
-function nothingToShow() {
-  alert("No items here yet.")
-}
-
-function resetSalesLog() {
-  const itemsSoldKeysArray = Object.keys(localStorage).filter(key => key.includes("_"));
-  itemsSoldKeysArray.forEach(item => {
-    localStorage.removeItem(item)
-  })
-}
+})();
 
 
-//MIDDLE SECTION
+//===================================================================================//
+
+
+//MIDDLE SECTION CODE
+
 //Show books from dropdown menu
 let options = '';
 
@@ -98,6 +80,8 @@ booksArray.forEach((book) => {
 });
 
 selectItem.innerHTML = options;
+
+//MIDDLE SECTION FUNCTIONS
 
 //Calculate total cost (subtotal + tax)
 function calcTotal() {
@@ -116,14 +100,12 @@ function clearSubtotal() {
 //Show item subtotal, tax, total, and remaining stock
 function showItemInfo() {
   let item = selectItem.value;
-
   const bookSelected = booksArray.find((book) => book.title === item);
-
   showSubtotal.innerHTML = `$${bookSelected.price}`;
   showTax.innerHTML = `${bookSelected.tax * 100}%`;
   const totalCost = bookSelected.price * bookSelected.tax + bookSelected.price;
   showTotal2.innerHTML = `$${totalCost.toFixed(2)}`;
-  showInventory.innerText = localStorage.getItem(bookSelected.title);
+  showRemainingStock.innerText = localStorage.getItem(bookSelected.title);
 }
 
 //Clear item info fields
@@ -131,14 +113,17 @@ function clearItemInfoFields() {
   showSubtotal.innerHTML =
     showTax.innerHTML =
     showTotal2.innerHTML =
-    showInventory.innerHTML =
+    showRemainingStock.innerHTML =
       '';
 }
 
 
-//LOWER SECTION - INVENTORY
+//===================================================================================//
 
-//Add book key-value pairs to storage only if they don't already exist
+
+//BOTTOM SECTION CODE
+
+//Add item key-value pairs to localStorage only if they don't already exist
 booksArray.forEach(book => {
   if(!localStorage.getItem(book.title)) {
     localStorage.setItem(book.title, book.quantity);
@@ -150,72 +135,70 @@ notebooksArray.forEach(notebook => {
 }})
 
 
-//Show pop-up modal with book inventory table
-function showModal() {
+//BOTTOM SECTION FUNCTIONS
+
+
+//MODAL FUNCTIONS
+//Show modals
+function showBookModal() {
   modalOverlay.classList.remove('hidden');
   modal.classList.remove('hidden');
   showBookInventory();
 }
 
-function showModal2() {
+function showNotebookModal() {
   modalOverlay.classList.remove('hidden');
   modal.classList.remove('hidden');
   showNotebookInventory();
 }
 
-function showModal4() {
+function showSalesModal() {
   modalOverlay.classList.remove('hidden');
   modal.classList.remove('hidden');
   showSalesRecord();
 }
 
-
-
-//Hide pop-up modal with book inventory table
+//Hide modals
 function hideModal() {
   modalOverlay.classList.add('hidden');
   modal.classList.add('hidden');
 }
 
-const date2 = new Date();
-let dateFormatted2 = date2.toLocaleString('en-US', {
-  weekday: 'long',
-  month: 'short',
-  day: 'numeric',
-  year: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric'
-});
-
-
-//Update item stock in localStorage and the UI
+//Update item stock in localStorage and the UI when user clicks on "Subtract" button in inventory table
 function updateStorage(e) {
+  if(e.target.className === 'btn-subtract-qty') {
   const targetBook = e.target.parentElement.parentElement.children[1].innerText;
   const currentStock = e.target.parentElement.parentElement.children[2].innerText;
   localStorage.setItem(targetBook, currentStock - 1)
   e.target.parentElement.parentElement.children[2].innerText = localStorage.getItem(targetBook);
   if(localStorage.getItem(targetBook) === "0") {
-    e.target.parentElement.parentElement.className="row-red"
+    e.target.parentElement.parentElement.className="row-red";
+    e.target.parentElement.firstChild.disabled = "true"
   } else if(localStorage.getItem(targetBook) === "1") {
     e.target.parentElement.parentElement.className="row-yellow"
   } else {
     e.target.parentElement.parentElement.className="row-green"
   }
-  
-  if(localStorage.getItem(targetBook) === "0") {
-    e.target.parentElement.firstChild.disabled = "true"
-  }
+  //Record each transaction in Sales Log
   recordSale(targetBook)
-}
+}}
 
-
-
+//Each key in local storage has to be unique, otherwise it's overwritten. Math.random() used to help with unique key
 function recordSale(item) {
   let unique = Math.floor(Math.random() * 200);
   localStorage.setItem(`${item}_${unique}`, dateFormatted)
 }
 
+function resetSalesLog() {
+  const itemsSoldKeysArray = Object.keys(localStorage).filter(key => key.includes("_"));
+  itemsSoldKeysArray.forEach(item => {
+    localStorage.removeItem(item)
+  })
+}
+
+function nothingToShow() {
+  alert("No items here yet.")
+}
 
 
 //SHOW INVENTORY TABLE FOR BOOKS
@@ -237,9 +220,7 @@ function showBookInventory() {
   <tbody>
   `;
 
-
   booksArray.forEach((book, index) => {
-
 
     let zeroExists = localStorage.getItem(book.title) === "0";
     let oneExists = localStorage.getItem(book.title) === "1";
@@ -265,6 +246,7 @@ function showBookInventory() {
   modal.insertAdjacentHTML('afterbegin', html);
 
 }
+
 
 //SHOW INVENTORY TABLE FOR NOTEBOOKS
 function showNotebookInventory() {
@@ -313,9 +295,6 @@ function showNotebookInventory() {
 }
 
 
-
-
-
 //SHOW SALES LOG
 function showSalesRecord() {
 
@@ -335,9 +314,7 @@ function showSalesRecord() {
   <tbody>
   `;
 
-  
   itemsSoldKeysArray.forEach((item, index) => {
-
 
     html += `
     <tr class="row-green">
